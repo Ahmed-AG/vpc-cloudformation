2dd3dd5658382b637e4287d1d5064402781cfdf7
Ahmad Abu gharbia <aag@Ahmads-MacBook-Pro.local>
2023-05-30 20:23:15 -0500

    Update Pipeline

--- bin/deploy_cfn.sh
+++ bin/deploy_cfn.sh
@@ -14,7 +14,7 @@ CAPABILITIES="CAPABILITY_IAM"
 # Check if stack exists
 if aws cloudformation describe-stacks --region "$REGION" --stack-name "$STACK_NAME" &>/dev/null; then
     echo "Stack $STACK_NAME exists. Updating..."
-    aws cloudformation update-stack --profile "$PROFILE" --region "$REGION" --stack-name "$STACK_NAME" --template-body "file://$TEMPLATE_FILE" --capabilities "$CAPABILITIES" --parameters ParameterKey=Region,ParameterValue=us-east-2 
+    aws cloudformation update-stack --profile "$PROFILE" --region "$REGION" --stack-name "$STACK_NAME" --template-body "file://$TEMPLATE_FILE" --capabilities "$CAPABILITIES" --parameters ParameterKey=AdminIPAddress,ParameterValue=$(curl https://ipinfo.io/ip)
 else
     echo "Stack $STACK_NAME does not exist. Creating..."
     aws cloudformation deploy --region "$REGION" --stack-name "$STACK_NAME" --template-file "$TEMPLATE_FILE" --parameter-overrides $PARAMETERS --capabilities "$CAPABILITIES"
