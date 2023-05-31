12f7fa4e5c73e798adc5bf6bff6d1e4b1f6e16e4
Ahmad Abu gharbia <aag@Ahmads-MacBook-Pro.local>
2023-05-30 20:35:38 -0500

    Update Pipeline

--- bin/deploy_cfn.sh
+++ bin/deploy_cfn.sh
@@ -8,13 +8,13 @@ REGION="us-east-2"
 STACK_NAME="my-demostack"
 TEMPLATE_FILE="main.yaml"
 
-PARAMETERS="AdminIPAddress=$(curl https://ipinfo.io/ip)/32"
+PARAMETERS="AdminIPAddress=$(curl -s https://ipinfo.io/ip)/32"
 CAPABILITIES="CAPABILITY_IAM"
 
 # Check if stack exists
 if aws cloudformation describe-stacks --region "$REGION" --stack-name "$STACK_NAME" &>/dev/null; then
     echo "Stack $STACK_NAME exists. Updating..."
-    aws cloudformation update-stack --profile "$PROFILE" --region "$REGION" --stack-name "$STACK_NAME" --template-body "file://$TEMPLATE_FILE" --capabilities "$CAPABILITIES" --parameters ParameterKey=AdminIPAddress,ParameterValue=$(curl https://ipinfo.io/ip)/32
+    aws cloudformation update-stack --profile "$PROFILE" --region "$REGION" --stack-name "$STACK_NAME" --template-body "file://$TEMPLATE_FILE" --capabilities "$CAPABILITIES" --parameters ParameterKey=AdminIPAddress,ParameterValue=$(curl -s https://ipinfo.io/ip)/32
 else
     echo "Stack $STACK_NAME does not exist. Creating..."
     aws cloudformation deploy --region "$REGION" --stack-name "$STACK_NAME" --template-file "$TEMPLATE_FILE" --parameter-overrides $PARAMETERS --capabilities "$CAPABILITIES"
